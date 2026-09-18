eaa9f37c450f8d399619005fe73df846d6dc7213
dlsantt <dleozin12@gmail.com>
2026-09-18 01:20:46 -0300

    home em processo

--- src/frontend-app/src/routes/+page.svelte
+++ src/frontend-app/src/routes/+page.svelte
@@ -1,31 +1,55 @@
 <script lang="ts">
   import Menu from '../components/Menu.svelte';
-  
+
 </script>
 
 <Menu />
 
+<div class="bg-[#ffdca0]"> 
+  <div class="bg-[#ffffff] border-2">
+    <img src="./images/baratieretro.png" alt="" class="h-full w-screen m-auto ">
+  </div>
 
-<div>
-  <img src="./images/taven.png" alt="" class="h-full w-screen">
+    <div class="sobre bg-[#deb266] h-screen w-400 shadow-2xl rounded-4xl m-auto mt-30">
+        <div class="grid grid-cols-2 gap-3]">
+
+        <div class="col-span-1  text-center">
+        <br>
+            <div class="">
+              <h1 class="mt-30 ">Seja bem-vindo ao ...!</h1>
+              <h2 class="mt-20 ">ier7gteuytrueyb6 <br>ietrythfeiu <br>gteryfhwiet69
+            </div>
+          </div>
+
+          <div class="col-span-1  overflow-hidden">
+            <img src="./images/taven.png" alt="" class="mt-20 display-block ml-auto mr-auto border-2 rounded-4xl w-130 transition-transform duration-700 ease-in-out hover:scale-110 ">
+          </div>
+
+      </div>
+    </div>
 </div>
 
-<div class="grid grid-cols-2 gap-3]">
 
-  <div class="col-span-1 bg-[#6c88eb] text-center">
+
+
+
+<!-- <div class="grid grid-cols-2 gap-3]">
+
+  <div class="col-span-1 bg-[#d7b377] text-center">
   <br><br><br>
-    <div class="border-5 rounded-4xl bg-[#ffffff]">
+    <div class="border-5 rounded-4xl bg-[#d7b377]">
       <h1 class="mt-30 ">Seja bem-vindo ao ...!</h1>
       <h2 class="mt-20 ">ier7gteuytrueyb6 <br>ietrythfeiu <br>gteryfhwiet69
     </div>
   </div>
 
-  <div class="col-span-1 bg-[#345645] overflow-hidden">
+  <div class="col-span-1 bg-[#d7b377] overflow-hidden">
     <img src="./images/taven.png" alt="" class="mt-20 display-block ml-auto mr-auto border-2 rounded-4xl w-130 transition-transform duration-700 ease-in-out hover:scale-110 ">
   </div>
 
 </div>
 
- <div class="col-span-1 bg-[#345645] text-center">ier7gteuytrueyb6 <br>ietrythfeiu <br>gteryfhwiet69</div>
-  <div class="col-span-1 bg-[#345645] text-center">ier7gteuytrueyb6 <br>ietrythfeiu <br>gteryfhwiet69</div>
-
+ <div class="col-span-1 bg-[#d7b377] text-center">ier7gteuytrueyb6 <br>ietrythfeiu <br>gteryfhwiet69</div>
+  <div class="col-span-1 bg-[#d7b377] text-center">ier7gteuytrueyb6 <br>ietrythfeiu <br>gteryfhwiet69</div>
+  bg-[#deb266]
+ -->
