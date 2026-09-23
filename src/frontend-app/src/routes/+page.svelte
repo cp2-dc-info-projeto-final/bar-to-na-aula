8d3921244a10283671ff1876d63feb44dccde804
dlsantt <dleozin12@gmail.com>
2026-09-22 23:38:34 -0300

    mexendo no home

--- src/frontend-app/src/routes/+page.svelte
+++ src/frontend-app/src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   import Menu from '../components/Menu.svelte';
-  // import { Carousel } from "flowbite-svelte";
+  import '../app.css';
 
 </script>
 
 <Menu />
 
 <div class="bg-[#deb266]"> 
-  <div class="bg-[#ffffff]">
+  <div class="bg-[#deb266]">
     <img src="./images/baratieretro.png" alt="" class="h-full w-screen m-auto shadow-2xl">
   </div>
-  <div class="bg-[#166a8e] h-screen w-350 shadow-2xl rounded-4xl m-auto mt-20 "><br>
-    <div class=" bg-[#deb266] h-150 w-285 shadow-2xl rounded-4xl m-auto ">
+  <div class="sobre bg-[#ffffff]"><br>
+    <!-- <div class=" h-150 w-285 shadow-2xl rounded-4xl m-auto "> -->
       <div class="grid grid-cols-2 gap-3]">
         <div class="col-span-1  text-center"><br>
           <div class="">
@@ -22,15 +22,10 @@
         <div class="col-span-1  overflow-hidden">
             <img src="./images/taven.png" alt="" class="mt-20 display-block ml-auto mr-auto border-2 rounded-4xl w-130 transition-transform duration-700 ease-in-out hover:scale-105 ">
         </div>
-      </div>
+      <!-- </div> -->
     </div>
   </div>
-
-  <!-- <Carousel {"foto1.png", "foto2.png"} duration={3000} /> -->
-
 </div>
-
-
 
 
 
